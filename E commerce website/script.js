8b6ea78ad8f99af0d64a980942e440c580dab962
yamini0425 <yaminiiiii04@gmail.com>
2024-10-06 13:49:12 +0530

    Add files via upload
    E-Commerce or Electronic Commerce means buying and selling of goods, products, or services over the internet. E-commerce is also known as electronic commerce or internet commerce. These services provided online over the internet network. Transaction of money, funds, and data are also considered as E-commerce.

---
+++ E commerce website/script.js
@@ -0,0 +1,19 @@
+var MainImage = document.getElementById("MainImage");
+var smallImage = document.getElementsByClassName("small-img");
+smallImage[0].onclick = function(){
+    MainImage.src = smallImage[0].src;
+}
+smallImage[1].onclick = function(){
+    MainImage.src = smallImage[1].src;
+}
+smallImage[2].onclick = function(){
+    MainImage.src = smallImage[2].src;
+}
+smallImage[3].onclick = function(){
+    MainImage.src = smallImage[3].src;
+}
+const addtocart = getElementsByClassName('pro-container');
+
+function addtocart(event)
+
+}
